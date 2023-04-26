33aebfe51ca981c10ca86f9c7b087ff88f53c5ee
Brian Bennett <brian.bennett2@gmail.com>
2023-04-26 12:58:04 -0400

    WIP

--- canvasapi/paginated_list.py
+++ canvasapi/paginated_list.py
@@ -56,17 +56,16 @@ class PaginatedList(object):
             self._request_method, self._next_url, **self._next_params
         )
         data = response.json()
-
         self._next_url = None
         # Check the response headers first. This is the normal Canvas convention
-        # for pagination, but there are edge case endpoints which return a `meta`
-        # property for pagination.
+        # for pagination, but there are edge case endpoints which return a `meta
+        # property for pagination.⏎
         # See https://github.com/ucfopen/canvasapi/discussions/605
         if response.links:
             next_link = response.links.get("next")
         elif type(data) is dict and data.get("meta") is not None:
-            # requests parses Link headers into a dict, this simply
-            # mirrors that structure so the regex searches will work.
+            # requests parses headers into dicts, this returns the same
+            # structure so the regex will work.
             next_link = {
                 "url": data.get("meta").get("pagination").get("next"),
                 "rel": "next"
@@ -89,7 +88,7 @@ class PaginatedList(object):
                 data = data[self._root]
             except KeyError:
                 # TODO: Fix this message to make more sense to an end user.
-                raise ValueError("The key {} does not exist in the response object.".format(self._root))
+                raise ValueError("The key <{}> does not exist in the response.".format(self._root))
 
         for element in data:
             if element is not None:
